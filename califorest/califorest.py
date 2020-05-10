6d55dda4c74fe8943ed1ff95282bb0d93b1c7460
Joyce Ho <joyce@ip-172-31-23-122.ec2.internal>
2020-05-10 21:11:31 +0000

    Changing califorest and rc30 to be compatible for use with scikit-learn's GridSearchCV

--- califorest/califorest.py
+++ califorest/califorest.py
@@ -2,8 +2,11 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier as Tree
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.isotonic import IsotonicRegression as Iso
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 
-class CaliForest:
+
+class CaliForest(ClassifierMixin, BaseEstimator):
 
     def __init__(self,
                 n_estimators=300,
@@ -15,37 +18,41 @@ class CaliForest:
                 alpha0=100,
                 beta0=25):
 
-        estimators = []
-        for i in range(n_estimators):
-            estimators.append(Tree(criterion=criterion,
-                                    max_depth=max_depth,
-                                    min_samples_split=min_samples_split,
-                                    min_samples_leaf=min_samples_leaf,
-                                    max_features="auto"))
-        self.estimators = estimators
-        self.calibrator = None
-        if ctype=="logistic":
-            self.calibrator = LR(penalty="none", 
-                                solver="saga", 
-                                max_iter=5000)
-        elif ctype=="isotonic":
-            self.calibrator = Iso(y_min=0, 
-                                y_max=1,
-                                out_of_bounds="clip")
+        self.n_estimators = n_estimators
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
         self.ctype = ctype
         self.alpha0 = alpha0
         self.beta0 = beta0
 
+    
     def fit(self, X, y):
-
+        X, y = check_X_y(X, y, accept_sparse=False)
+        self.estimators = []
+        self.calibrator = None
+        for i in range(self.n_estimators):
+            self.estimators.append(Tree(criterion=self.criterion,
+                                        max_depth=self.max_depth,
+                                        min_samples_split=self.min_samples_split,
+                                        min_samples_leaf=self.min_samples_leaf,
+                                        max_features="auto"))
+        if self.ctype=="logistic":
+            self.calibrator = LR(penalty="none",
+                                 solver="saga",
+                                 max_iter=5000)
+        elif self.ctype=="isotonic":
+            self.calibrator = Iso(y_min=0,
+                                y_max=1,
+                                out_of_bounds="clip")
         n, m  = X.shape
-        n_est = len(self.estimators)
-        Y_oob = np.full((n, n_est), np.nan)
+        Y_oob = np.full((n, self.n_estimators), np.nan)
         n_oob = np.zeros(n)
-        IB = np.zeros((n, n_est), dtype=int)
-        OOB = np.full((n, n_est), True)
-
-        for eid in range(n_est):
+        IB = np.zeros((n, self.n_estimators), dtype=int)
+        OOB = np.full((n, self.n_estimators), True)
+        
+        for eid in range(self.n_estimators):
             IB[:,eid] = np.random.choice(n, n)
             OOB[IB[:,eid],eid] = False
 
@@ -56,22 +63,29 @@ class CaliForest:
             Y_oob[oob_idx,eid] = est.predict_proba(X[oob_idx,:])[:,1]
             n_oob[oob_idx] += 1
 
-        Y_oob_ = Y_oob[n_oob > 0,:]
+
+        oob_idx = n_oob > 1
+        Y_oob_ = Y_oob[oob_idx,:]
+        n_oob_ = n_oob[oob_idx]
         z_hat = np.nanmean(Y_oob_, axis=1)
+        z_true = y[oob_idx]
 
-        z_true = y[n_oob > 0]
-
-        beta = self.beta0 + np.nanvar(Y_oob_, axis=1) * n_oob / 2
-        alpha = self.alpha0 + n_oob/2
+        beta = self.beta0 + np.nanvar(Y_oob_, axis=1) * n_oob_ / 2
+        alpha = self.alpha0 + n_oob_/2
         z_weight = alpha / beta
 
         if self.ctype=="logistic":
             self.calibrator.fit(z_hat[:,np.newaxis], z_true, z_weight)
         elif self.ctype=="isotonic":
             self.calibrator.fit(z_hat, z_true, z_weight)
+        self.is_fitted_ = True
+        return self
 
+    
     def predict_proba(self, X):
-
+        X = check_array(X)
+        check_is_fitted(self, 'is_fitted_')
+        
         n, m = X.shape
         n_est = len(self.estimators)
         z = np.zeros(n)
@@ -84,11 +98,14 @@ class CaliForest:
             y_mat[:,1] = self.calibrator.predict_proba(z[:,np.newaxis])[:,1]
         elif self.ctype=="isotonic":
             y_mat[:,1] = self.calibrator.predict(z)
+            
         y_mat[:,0] = 1 - y_mat[:,1]
-        
         return y_mat
 
 
+    def predict(self, X):
+        proba = self.predict_proba(X)
+        return np.argmax(proba, axis=1)
 
 
 
--- califorest/rc30.py
+++ califorest/rc30.py
@@ -4,7 +4,11 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.isotonic import IsotonicRegression
 from sklearn.model_selection import train_test_split
 
-class RC30:
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+
+
+class RC30(ClassifierMixin, BaseEstimator):
 
     def __init__(self, 
                 n_estimators=30, 
@@ -12,18 +16,23 @@ class RC30:
                 min_samples_split=2,
                 min_samples_leaf=1, 
                 ctype="isotonic"):
-        self.model = RandomForestClassifier(n_estimators=n_estimators,
-                                    max_depth=max_depth,
-                                    min_samples_split=min_samples_split,
-                                    min_samples_leaf=min_samples_leaf)
-        if ctype == "logistic":
-            self.calibrator = LogisticRegression(C=1e20, solver="lbfgs")
-        elif ctype == "isotonic":
-            self.calibrator = IsotonicRegression(y_min=0, y_max=1,
-                                                out_of_bounds="clip")
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
         self.ctype = ctype
 
     def fit(self, X, y):
+        X, y = check_X_y(X, y)
+        self.model = RandomForestClassifier(n_estimators=self.n_estimators,
+                                            max_depth=self.max_depth,
+                                            min_samples_split=self.min_samples_split,
+                                            min_samples_leaf=self.min_samples_leaf)
+        if self.ctype == "logistic":
+            self.calibrator = LogisticRegression(C=1e20, solver="lbfgs")
+        elif self.ctype == "isotonic":
+            self.calibrator = IsotonicRegression(y_min=0, y_max=1,
+                                                out_of_bounds="clip")
         X0, X1, y0, y1 = train_test_split(X, y, test_size=0.3) 
         self.model.fit(X0, y0)
         if self.ctype == "logistic":
@@ -32,8 +41,14 @@ class RC30:
         elif self.ctype == "isotonic":
             y_est = self.model.predict_proba(X1)[:,1]
             self.calibrator.fit(y_est, y1)
+
+        self.is_fitted_ = True
+        return self
  
     def predict_proba(self, X):
+        X = check_array(X)
+        check_is_fitted(self, 'is_fitted_')
+        
         if self.ctype == "logistic":
             return self.calibrator.predict_proba(
                     self.model.predict_proba(X)[:,[1]])
